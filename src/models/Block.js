7b2cfede27648fc2c8f4eca453710f69925abdfc
ShallWeKissForever <132680922+ShallWeKissForever@users.noreply.github.com>
2023-05-10 22:49:35 +0800

    lesson1

--- src/models/Block.js
+++ src/models/Block.js
@@ -2,8 +2,22 @@ class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
 
-  */
-  constructor() {}
+    // 构造方法
+    */
+  constructor(blockchain, previousHash,height,hash) {
+      //属于哪条链
+      this.blockChain=blockchain;
+      // 父区块的哈希值
+      this.previousHash = previousHash;
+      //当且区块的高度
+      this.height=height;
+      // 当前区块的哈希值
+      this.hash = hash;
+      // 此区块的nonce值，用于工作量证明算法的计算
+      this.nonce = 0;
+      //此区块储存的数据的默克尔树根
+      this.merkleRoot='';
+    }
 }
 
 export default Block
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -6,17 +6,38 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
-
-  // 2. 定义 longestChain 函数
-  /* 
-    返回当前链中最长的区块信息列表
-  */
-  longestChain() {
-
-
-    return []
+  constructor(name) {
+    // 区块链的名字
+    this.name = name;
+    //创世区块
+    this.genesis=''
+    // 用于存储所有区块的映射
+    this.blocks = {};
   }
+    // 2. 定义 longestChain 函数
+    /* 
+      返回当前链中最长的区块信息列表
+    */
+    longestChain() {
+      // 定义一个空数组保存最长的一条链。
+      let chain=new Array();
+      // 初始值设定为创世区块，表示链的起始。
+      let longestBlock=this.genesis;
+      // 遍历所有区块以找到最长的区块
+      for(let tempBlock of Object.values(this.blocks)){
+        if(longestBlock.height<tempBlock.height)longestBlock=tempBlock;
+      }
+      // 取得最长区块的哈希值
+      let hash=longestBlock.hash;
+      // 把当前区块加入链的起始位置，直到最长区块的哈希值是创世区块的哈希值为止
+      while(hash!=this.genesis.hash){
+        chain.unshift(this.blocks[hash]);
+        hash=this.blocks[hash].previousHash;
+      } 
+      // 返回最长的一条链
+      return chain;
+        
+    }
 }
 
 export default Blockchain
